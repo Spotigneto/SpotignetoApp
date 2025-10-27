baf0f80f9ddff5dbb1bf14e653cc4f76d91d343d
Davide Gaetano <davidegaetano25@gmail.com>
2025-10-27 16:49:59 +0100

    Pagina libreria e playlist aggiornata con nuove funzionalità e correzioni di bug.

--- Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09936229063bc8ec17a8ccfe6a01ec3909c9dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa1134ce677ec788ea608bcfeaa5e32919c5e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
