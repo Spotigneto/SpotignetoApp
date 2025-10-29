a42e8c924a4a82c191fe0be9f5bf5f083a35cd56
MatteoCrielesi <crielesimatteo@gmail.com>
2025-10-29 11:26:35 +0100

    sistemato crea playlist

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0851909c618b7447f00aa269db0f08d6d7d24246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546cb7d933fd57e7f726e4932f0695a59fb57e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
