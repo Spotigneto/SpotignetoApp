3e0902ab18232833bbe16d7fc4d0754abe184326
dani20041502 <daniele.bianchi2004@gmail.com>
2025-10-30 11:42:06 +0100

    css del bottone login bianco nel navmenu

--- Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7502fa0ded607d978d3e090c2f84a74133ea48b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206761983feca50d6e1e1802f00794193a82d891")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
